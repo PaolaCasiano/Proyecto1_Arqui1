91883aeddc8335b007ceeb907c3bc095cbff066b
MAX F.C <maxgt734@gmail.com>
2019-09-16 17:44:35 -0600

    maincra xdd

--- ARQUI1_P1/app/src/main/java/com/example/arqui1_p1/Main4Activity.java
+++ ARQUI1_P1/app/src/main/java/com/example/arqui1_p1/Main4Activity.java
@@ -30,23 +30,37 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 
 public class Main4Activity extends Activity  {
-    Button btnEliminar;
+    Button btnEliminar,btnImprimir;
     ArrayList<Imagen> f = new ArrayList<>();
     File[] listFile;
     String pathtemp;
+    String url = "http://192.168.43.240:8000/servidorApp/buscar_imagen";
+    RequestQueue rq;
+    String nombre = "";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        rq = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main4);
         getImagenes();
         GridView gridView = findViewById(R.id.myGrid);
         gridView.setAdapter(new imageAdapter(f,this));
+        btnImprimir = (Button) findViewById(R.id.btn_imprimir);
         btnEliminar = (Button)findViewById(R.id.btn_eliminar);
         btnEliminar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +79,34 @@ public class Main4Activity extends Activity  {
                 int posactual=f.get(i).id;
                 Log.e("POSICION","La posicion es: "+posactual);
                 pathtemp=f.get(posactual).path;
+            }
+        });
+        btnImprimir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String[] aux = pathtemp.split("/");
+                    nombre = aux[aux.length];
+                StringRequest str = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),"Error:"+error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String,String> getParams(){
+                        Map<String,String> params = new HashMap<>();
+                        params.put("nombre",nombre);
+                        return params;
+                    }
+                };
+                rq.add(str);
+                Toast.makeText(getApplicationContext(),"Imagen Imprimiendose...",Toast.LENGTH_SHORT).show();
             }
         });
     }
